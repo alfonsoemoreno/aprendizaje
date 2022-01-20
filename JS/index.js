d66ab8e78b9392a7d5878f7e325d9bd076ca0da3
Alfonso Moreno <zinger.unsold.0b@icloud.com>
2022-01-19 23:59:49 -0300

    Initial index.js

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+console.log('Hello World')
+
+let thisIsAString = 'This is a string type'
+let isABoolean = true
+let isANumber = 12
+let isAnUndefinied
+let isANull = null
+
+console.log(thisIsAString, isABoolean, isANumber, isAnUndefinied, isANull)
+
+const aPerson = {
+    name: 'Alfonso',
+    lastName: 'Moreno',
+    age: 38,
+    isMarried: true,
+    email: 'alfonso.moreno@me.com',
+}
+
+console.log(aPerson)
+
+console.log('Hello', aPerson.name, aPerson.lastName)
