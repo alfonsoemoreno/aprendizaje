68c8c7dc53a675b3e3b280a2e844f3fc2b3aa290
Alfonso Moreno <zinger.unsold.0b@icloud.com>
2022-01-21 08:56:28 -0300

    Python introduction

---
+++ python/index.py
@@ -0,0 +1,27 @@
+# Acá va un comentario
+if 3 > 5: # Acá va un comentario
+    print('Esto no se va a imprimir')
+
+if 5 > 3: 
+    # print('5 es mayor a 3')
+
+    x = 5
+    y = 'chanchito feliz'
+    # print(x, y)
+
+    email = 'chanchito@feliz.com'
+    # print(email)
+
+    miVar = 'chanchito'
+
+# Multiples variables
+a, b, c = 'Lala', 'Lele', 'Lili'
+# print(a, b, c)
+
+valor1 = valor2 = valor3 = 'chanchito feliz'
+# print(valor1, valor2, valor3)
+
+# Concatenación
+inicio = 'Hola '
+final = 'mundo'
+print(inicio +  final)
