ad19b2f498fbc7904d5c9f34178b699bdc4a1f0f
Alfonso Moreno <zinger.unsold.0b@icloud.com>
2022-01-21 12:28:02 -0300

    Sección: Control de flujo

---
+++ python/controlflujo.py
@@ -0,0 +1,37 @@
+# Control de flujo: if
+# if 2 < 5:
+    # print('2 es menor que 5')
+
+# a == b
+# a < b
+# a > b
+# a != b
+# a <= b
+# a >= b
+
+# if 2 == 2:
+    # print('2 es igual a 2')
+
+# if 2 == 3:
+    # print('2 no es igual a 3')
+
+# if 2 > 5:
+#     print("No")
+# elif 2 == 5:
+#     print("2 es menor a 5 en elif")
+# else:
+#     print("Las instrucciones anteriores no sirvieron")
+
+# if cortos
+# if 2 < 5: print("if de una linea")
+
+# Evaluación ternaria
+# print("Cuando devuelva True") if 5 > 2 else print("Cuando devuelva False")
+# print("Cuando devuelva True") if 5 < 2 else print("Cuando devuelva False")
+
+# AND y OR
+if 2 < 5 and 3 > 2:
+    print("Se cumplen ambas condiciones")
+
+if 3 < 2 or 2 == 2:
+    print("Una condición se cumple")
