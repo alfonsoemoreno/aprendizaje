658b0daea5c31938727e09ed2b133de57e61afd0
Alfonso Moreno <zinger.unsold.0b@icloud.com>
2022-03-15 09:07:14 -0300

    New files

---
+++ JS/intro-dom/main.js
@@ -0,0 +1,40 @@
+const todos = [];
+
+const render = () => {
+    const todoList = document.getElementById('todo-list')
+    todoList.innerHTML = '';
+    // for (let i = 0; i < todos.length; i++) {
+    //     todoList.innerHTML += '<li>'+ todos[i] +'</li>';
+    // }
+    const todoTemplate = todos.map(t => '<li>' + t + '</li>');
+    todoList.innerHTML = todoTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li')
+    elementos.forEach((elemento, i) => {
+        // console.log(elemento, i);
+        elemento.addEventListener('click', () => {
+            // console.log(elemento, i);
+            elemento.parentNode.removeChild(elemento);
+            todos.splice(i, 1);
+            render();
+        })
+    })
+}
+
+window.onload = () => {
+    // console.log("Intro a DOM desde main.js")
+
+    // const parrafo = document.getElementById('text')
+    // console.log(parrafo.innerHTML)
+    // parrafo.innerText = "Texto actualizado"
+    // parrafo.innerHTML += '<ul><li>Elemento 1</li><li>Elemento 2</li></ul>'
+    const form = document.getElementById('todo-form');
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById('todo')
+        const todoText = todo.value;
+        todo.value = '';
+        todos.push(todoText)
+        console.log(todoText)
+        render();
+    }
+}
